f39ff45b80d4296d108826af9178657287f84b05
KIHLYouns <kihlyounes123@gmail.com>
2024-09-17 23:10:40 +0100

    update

--- src/main/java/com/dev/inventoryManagement/config/WebConfig.java
+++ src/main/java/com/dev/inventoryManagement/config/WebConfig.java
@@ -1,20 +1,31 @@
 // WebConfig.java
 package com.dev.inventoryManagement.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Adjust the path pattern if needed
-                .allowedOrigins(
-                        "*"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
-                .allowedHeaders("*"); // Allowed headers
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        // Allow credentials to true if your frontend needs to send cookies or authentication information
+        configuration.setAllowCredentials(true);
+        // Set allowed origins to specific domains or use a custom source to match against a pattern
+        configuration.setAllowedOriginPatterns(Collections.singletonList("https://*.onrender.com"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+        return source;
     }
 }
